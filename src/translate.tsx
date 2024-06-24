8ad03f1b439e4d4357758474d43a8a11ca2f955b
bviigp32 <bviigp321@naver.com>
2024-06-24 16:41:15 +0900

    Applying TypeScript to Context

--- src/translate.tsx
+++ src/translate.tsx
@@ -1,18 +1,25 @@
-import { createContext, useContext, useState } from 'react';
+import { ReactNode, createContext, useContext, useState } from "react";
 
-const LocaleContext = createContext({
-  locale: 'ko',
+type Locale = "ko" | "en";
+
+interface LocaleContextValue {
+  locale: Locale;
+  setLocale: (value: Locale) => void;
+}
+
+const LocaleContext = createContext<LocaleContextValue>({
+  locale: "ko",
   setLocale: () => {},
-} as any);
+});
 
-export function LocaleContextProvider({ children }: any) {
-  const [locale, setLocale] = useState('ko');
+export function LocaleContextProvider({ children }: { children: ReactNode }) {
+  const [locale, setLocale] = useState<Locale>("ko");
 
   return (
     <LocaleContext.Provider
       value={{
         locale,
-        setLocale: setLocale as any,
+        setLocale,
       }}
     >
       {children}
@@ -20,24 +27,24 @@ export function LocaleContextProvider({ children }: any) {
   );
 }
 
-const dict = {
+const dict: { [key in Locale]: Record<string, string> } = {
   ko: {
-    signin: '로그인',
-    username: '아이디',
-    'email or phone number': 'Email 또는 전화번호',
-    password: '비밀번호',
-    'forgot your password?': '비밀번호를 잊으셨나요?',
-    'new user?': '회원이 아니신가요?',
-    signup: '가입하기',
+    signin: "로그인",
+    username: "아이디",
+    "email or phone number": "Email 또는 전화번호",
+    password: "비밀번호",
+    "forgot your password?": "비밀번호를 잊으셨나요?",
+    "new user?": "회원이 아니신가요?",
+    signup: "가입하기",
   },
   en: {
-    signin: 'Signin',
-    username: 'Username',
-    'email or phone number': 'Email or phone number',
-    password: 'Password',
-    'forgot your password?': 'Forgot your password?',
-    'new user?': 'New user?',
-    signup: 'Signup',
+    signin: "Signin",
+    username: "Username",
+    "email or phone number": "Email or phone number",
+    password: "Password",
+    "forgot your password?": "Forgot your password?",
+    "new user?": "New user?",
+    signup: "Signup",
   },
 };
 
@@ -51,9 +58,8 @@ export function useSetLocale() {
   return setLocale;
 }
 
-export function useTranslate(): (key: string) => string {
+export function useTranslate(): (key: keyof (typeof dict)["ko"]) => string {
   const locale = useLocale();
-  // @ts-ignore
-  const t = (key) => dict[locale][key];
+  const t = (key: keyof typeof dict[Locale]) => dict[locale][key];
   return t;
 }
